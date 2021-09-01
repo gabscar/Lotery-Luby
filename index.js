4fd9c246cb958a8e2071261739d76f1d764cf570
gabscar <srgabsc@gmail.com>
2021-09-01 15:59:39 -0300

    feat: adicionando as funcionalidades de random game e clear game

--- index.js
+++ index.js
@@ -2,75 +2,151 @@
     
 
    const lotery= (function(){
+        const numberBTNCollor ='#ADC0C4'; 
         const ajax = new XMLHttpRequest();
-        
-        let selectedNumbers =[]
+        let modelSelect;
+        let selectedNumbers =[];
         return{
             start: function(){
                 this.gameSelect();
             },
 
             gameSelect: function(){
-                ajax.open('GET','./games.json',true);
+                ajax.open('GET','./games.json');
                 ajax.send();
                 ajax.addEventListener('readystatechange',this.getGameRule)
             },
 
-            getGameRule:function(){
-                
+            getGameRule:function(){                
                 if(this.status ===200 && this.readyState ===4){
                     let data = JSON.parse(this.responseText)
-                    lotery.chooseMode(data.types,true);
+                    lotery.chooseAction(data.types);
                 }
             },
 
-            chooseMode: function(dataGame,active){
-                const sectionBtnRules = document.querySelector(".gameMode");
+            chooseAction: function(dataGame){
 
-                dataGame.map((item,index) => {
-                    selectedNumbers = [];
-                    let button =  document.createElement('button');
-                    button.setAttribute('class','btn-game-mode');
-                    button.setAttribute('selected', 'false')
-                    lotery.buttonGameStyle(button.style,item.color);      
-                    button.innerHTML = item.type;
-                    button.addEventListener('click',(evt)=>{
-                        evt.preventDefault();
-                        console.log(evt.target)
-                        button.style.backgroundColor = item.color;
-                        button.style.color = '#fff';
-                        button.setAttribute('selected','true');
-                        let listBtn = document.querySelectorAll('.btn-game-mode');
-                        listBtn.forEach((item)=>{
-                            if(item !== evt.target){
-                                if(item.getAttribute('selected') ==='true'){
-                                    let background = item.style.color;
-                                    item.style.color = item.style.backgroundColor;
-                                    item.style.backgroundColor = background;
-                                    item.setAttribute('selected','false');
-                                }
-                               
-                                //console.log(item)
-                            }
-                        })
-                    })
+                const sectionBtnRules = document.querySelector(".gameMode");
+                const btnClear = document.querySelector("[data-js = btn-clear-game]");
+                const btnRandGame = document.querySelector("[data-js = btn-random-game]");
+
+
+                lotery.selectModel(dataGame,sectionBtnRules);
+                btnClear.addEventListener('click',this.clearBalls);
+                btnRandGame.addEventListener('click',this.randomGame);
+
+            },
+
+            randomGame:function(){
+                let buttons = document.querySelectorAll("[data-js = numbersBtn]");    
+                    if(modelSelect==undefined){
+                        window.alert("Selecione um modo de jogo");
+                        return;
+                    }
+                  
+                    let limit = Number(modelSelect['max-number']) - selectedNumbers.length
+                    if(limit===0){
+                        this.clearBalls();
+                        limit = Number(modelSelect['max-number']);
+                    }
+                       
+                    let counter=0;
+                    while(counter<limit){
+                        let sort = Math.floor(Math.random() * (modelSelect.range));
+                        if(selectedNumbers.indexOf(sort) == -1){
+                            selectedNumbers.push(sort);
+                            counter++;
+                        }
+                    }
                     
-                    sectionBtnRules.appendChild(button);
-                   
-                    //console.log(item)
+                    buttons.forEach((item)=>{
+                        if(selectedNumbers.indexOf(Number(item.id)) !=-1){
+                            item.style.backgroundColor = modelSelect.color;
+                            item.setAttribute('selected','true');
+                        }                        
+                    });
+            },
+
+            clearBalls:function(){
+                let buttons = document.querySelectorAll("[data-js = numbersBtn]");                    
+                buttons.forEach((item)=>{
+                    item.style.backgroundColor = numberBTNCollor;
+                    item.setAttribute('selected','false');
+                    selectedNumbers = [];
                 });
             },
 
-            buttonGameStyle:function(button,color){
+            selectModel:function(dataGame,sectionBtnRules){
+                
+                dataGame.map((item,index) => {
+                    selectedNumbers = [];
+                    let $button =  document.createElement('button');
+                    $button.setAttribute('class','btn-game-mode');
+                    $button.setAttribute('selected', 'false');
+                    lotery.buttonGameModeStyle($button.style,item.color);      
+                    $button.innerHTML = item.type;
+                    $button.addEventListener('click',(evt)=>{
+                        evt.preventDefault();            
+                        selectedNumbers = [];            
+                        $button.style.backgroundColor = item.color;
+                        $button.style.color = '#fff';
+                        $button.setAttribute('selected','true');
+                        let listBtn = document.querySelectorAll('.btn-game-mode');
+                        listBtn.forEach((item)=>{
+                            if(item !== evt.target && item.getAttribute('selected') ==='true' ){
+                                let background = item.style.color;
+                                item.style.color = item.style.backgroundColor;
+                                item.style.backgroundColor = background;
+                                item.setAttribute('selected','false');                         
+                            }
+                        })
+                        modelSelect = item;
+                        lotery.generateTableGame(item);
+                    })
+                    
+                    sectionBtnRules.appendChild($button);
+                });
+            },
+
+            buttonGameModeStyle:function(button,color){
                 button.color = color;
                 button.backgroundColor='#FFFFFF';
                 button.border =`2px solid ${color}`;
                 button.padding = "0.3rem 1.5rem";
                 button.borderRadius = '6rem';
                 button.marginLeft = '1.5rem';
+            },
+
+            generateTableGame: function(element){
+                const $ruleText = document.querySelector("[data-js = textRule]"); 
+                const $gameNumbers = document.querySelector("[data-js = gameNumbers]"); 
                
-                
-            }
+                $gameNumbers.innerHTML=""
+                $ruleText.innerHTML = element.description;
+
+                for(let i=0;i<element.range;i++){
+                    const $button  = document.createElement('button');
+                    $button.setAttribute('selected','false');
+                    $button.setAttribute('id',i);
+                    $button.setAttribute('data-js','numbersBtn');
+                    $button.innerHTML = i>10? i:"0"+i;
+                    $button.addEventListener('click',()=>{
+                        let limit = element['max-number'] - selectedNumbers.length;
+                        if($button.getAttribute('selected') === 'false' && limit!==0){                           
+                            selectedNumbers.push(Number($button.getAttribute('id')));
+                            $button.setAttribute('selected','true');
+                            $button.style.backgroundColor = element.color;            
+                        }else if($button.getAttribute('selected') === 'true'){
+                            let removedIndex = selectedNumbers.indexOf(Number($button.getAttribute('id')));
+                            selectedNumbers.splice(removedIndex,1);
+                            $button.setAttribute('selected',false);
+                            $button.style.backgroundColor =numberBTNCollor;
+                        }
+                        console.log(selectedNumbers);
+                    })
+                    $gameNumbers.appendChild($button);
+                }
+            },
         }
     })();
     lotery.start();
